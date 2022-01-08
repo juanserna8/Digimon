8d0a1ba53e3929f2e2fb909c24fb3230ce4f2cad
Juan David Serna <sernadominguezj@gmail.com>
2022-01-09 10:39:00 +1100

    The image has been fixed. The local storage is rendering with its initial value

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const addButton = document.querySelector('#addButton');
 let initialNumberCont = document.querySelector('#initialNumberCont');
 initialNumber = initialNumberCont.textContent;
+initialNumberCont.innerText = localStorage.initialNumber;
 
 addButton.addEventListener("click", () => {
     //initialNumberCont.innerHTML = initialNumber++;
@@ -9,6 +10,5 @@ addButton.addEventListener("click", () => {
     } else {
         localStorage.initialNumber = 0;
     }
-    initialNumberCont.innerHTML = localStorage.initialNumber;
+    initialNumberCont.innerText = localStorage.initialNumber;
 });
-
