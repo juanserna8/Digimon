576227d728285a27279236a83fc6972349c9a824
Juan David Serna <sernadominguezj@gmail.com>
2022-01-08 20:33:10 +1100

    added the local storage function that stores the current number

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let initialNumberCont = document.querySelector('#initialNumberCont');
 initialNumber = initialNumberCont.textContent;
 
 addButton.addEventListener("click", () => {
-    initialNumberCont.innerHTML = initialNumber++;
+    //initialNumberCont.innerHTML = initialNumber++;
+    if(localStorage.initialNumber) {
+        localStorage.initialNumber = Number(localStorage.initialNumber)+1; 
+    } else {
+        localStorage.initialNumber = 0;
+    }
+    initialNumberCont.innerHTML = localStorage.initialNumber;
 });
 
